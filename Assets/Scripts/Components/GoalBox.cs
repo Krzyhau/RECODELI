5c1f1f567c6b06d91b32ffbacf8b19e12eca1d25
Krzyhau <krzyht@gmail.com>
2023-05-01 12:52:48 +0200

    goal box

---
+++ Assets/Scripts/Components/GoalBox.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LudumDare.Scripts.Components
+{
+    public class GoalBox : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            // TODO: wykryæ czy to gracz i gg
+        }
+    }
+}
