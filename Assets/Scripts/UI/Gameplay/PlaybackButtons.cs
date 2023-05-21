e3f0752350dd9be30dcf3f4d32f9d7bac1a88c05
Krzyhau <krzyht@gmail.com>
2023-05-21 11:55:26 +0200

    add timer

--- Assets/Scripts/UI/Gameplay/PlaybackButtons.cs
+++ Assets/Scripts/UI/Gameplay/PlaybackButtons.cs
@@ -1,6 +1,7 @@
 using RecoDeli.Scripts.Controllers;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace RecoDeli.Scripts.UI
@@ -14,6 +15,10 @@ namespace RecoDeli.Scripts.UI
         [SerializeField] private GameObject pauseButton;
         [SerializeField] private GameObject resumeButton;
         [SerializeField] private GameObject stopButton;
+        [Header("Timer")]
+        [SerializeField] private GameObject timerContainer;
+        [SerializeField] private TMP_Text timerText;
+        [SerializeField] private string timerFormat = "0.000";
 
         private void Update() 
         {
@@ -23,11 +28,26 @@ namespace RecoDeli.Scripts.UI
             bool pauseState = simulationManager.PlayingSimulation && !simulationManager.PausedSimulation;
             bool resumeState = simulationManager.PlayingSimulation && simulationManager.PausedSimulation;
             bool stopState = simulationManager.PlayingSimulation;
+            bool timerTextState = simulationManager.PlayingSimulation;
 
             if(playButton.activeSelf != playState) playButton.SetActive(playState);
             if(pauseButton.activeSelf != pauseState) pauseButton.SetActive(pauseState);
             if(resumeButton.activeSelf != resumeState) resumeButton.SetActive(resumeState);
             if(stopButton.activeSelf != stopState) stopButton.SetActive(stopState);
+            if(timerContainer.activeSelf != timerTextState) timerContainer.SetActive(timerTextState);
+
+            UpdateTimer();
+        }
+
+        private void UpdateTimer()
+        {
+            var monospaceSize = 0.6f;
+            var time = simulationManager.SimulationTime;
+            var timeString = time.ToString(timerFormat);
+            var timeSegments = timeString.Split('.');
+
+            var timerString = $"<mspace={monospaceSize}em>{timeSegments[0]}</mspace>.<mspace={monospaceSize}em>{timeSegments[1]}</mspace>";
+            timerText.text = timerString;
         }
     }
 }
