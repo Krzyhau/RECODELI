201c6884662d6e75adaf8d6cf40a738ffd574fd4
Krzyhau <krzyht@gmail.com>
2024-02-07 14:22:24 +0100

    handle saves with prefs on webgl

--- Assets/Scripts/SaveManagement/SaveManager.cs
+++ Assets/Scripts/SaveManagement/SaveManager.cs
@@ -4,6 +4,7 @@ using System;
 using UnityEngine;
 using RecoDeli.Scripts.Level.Format;
 using System.Xml;
+using System.Text;
 
 namespace RecoDeli.Scripts.SaveManagement
 {
@@ -38,22 +39,49 @@ namespace RecoDeli.Scripts.SaveManagement
             return Application.persistentDataPath + "/SaveData" + slot + ".xml";
         }
 
+        public static string GetPrefsSaveSlotKey(int slot)
+        {
+            return "SaveData" + slot;
+        }
+
+        public static bool ShouldUsePlayerPrefs()
+        {
+            return Application.platform == RuntimePlatform.WebGLPlayer;
+        }
+
         public static bool TryLoadSlot(int slot, out SaveData data)
         {
-            var path = GetSaveSlotPath(slot);
-
-            if (!File.Exists(path))
-            {
-                data = null;
-                return false;
-            }
-
             try
             {
                 var serializer = new XmlSerializer(typeof(SaveData));
-                using var fileStream = new FileStream(path, FileMode.Open);
 
-                data = (SaveData)serializer.Deserialize(fileStream);
+                if (ShouldUsePlayerPrefs())
+                {
+                    var prefsKey = GetPrefsSaveSlotKey(slot);
+
+                    if (!PlayerPrefs.HasKey(prefsKey))
+                    {
+                        data = null;
+                        return false;
+                    }
+
+                    using var stringReader = new StringReader(PlayerPrefs.GetString(prefsKey));
+                    data = (SaveData)serializer.Deserialize(stringReader);
+                }
+                else
+                {
+                    var path = GetSaveSlotPath(slot);
+
+                    if (!File.Exists(path))
+                    {
+                        data = null;
+                        return false;
+                    }
+
+                    using var fileStream = new FileStream(path, FileMode.Open);
+                    data = (SaveData)serializer.Deserialize(fileStream);
+                }
+
                 return true;
             }
             catch
@@ -99,15 +127,26 @@ namespace RecoDeli.Scripts.SaveManagement
 
             try
             {
-                using var fileStream = new FileStream(GetSaveSlotPath(currentSlot), FileMode.Create);
+                var saveBuilder = new StringBuilder();
 
-                XmlWriter xmlWriter = XmlWriter.Create(fileStream, new XmlWriterSettings()
+                var xmlWriter = XmlWriter.Create(saveBuilder, new XmlWriterSettings()
                 {
                     OmitXmlDeclaration = true,
                     Indent = true
                 });
                 var namespaces = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
                 new XmlSerializer(typeof(SaveData)).Serialize(xmlWriter, save, namespaces);
+
+                var saveString = saveBuilder.ToString();
+
+                if (ShouldUsePlayerPrefs())
+                {
+                    PlayerPrefs.SetString(GetPrefsSaveSlotKey(currentSlot), saveString);
+                }
+                else
+                {
+                    File.WriteAllText(GetSaveSlotPath(currentSlot), saveString);
+                }
 
                 Debug.Log("Save data saved successfully.");
             }
